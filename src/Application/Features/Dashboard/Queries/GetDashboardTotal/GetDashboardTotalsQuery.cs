f527d44bac3772ff8d46fbb06708a452505eecd1
khushalkhan9288@gmail.com <khushalkhan9288@gmail.com>
2024-08-09 00:01:27 +0500

    Total Campaigns with Campaign Id

--- src/Application/Features/Dashboard/Queries/GetDashboardTotal/GetDashboardTotalsQuery.cs
+++ src/Application/Features/Dashboard/Queries/GetDashboardTotal/GetDashboardTotalsQuery.cs
@@ -4,6 +4,7 @@ namespace CleanArchitecture.Blazor.Application.Features.Dashboard.Queries.GetDas
 
 public record GetDashboardTotalsQuery : ICacheableRequest<DashboardTotalsDto>
 {
+    public int CampaignId { get; set; }
     public MemoryCacheEntryOptions? Options => DashboardCacheKey.MemoryCacheEntryOptions;
     public string CacheKey => DashboardCacheKey.DashboardTotalsCacheKey;
 
@@ -20,8 +21,11 @@ public class GetDashboardTotalsQueryHandler : IRequestHandler<GetDashboardTotals
 
     public async Task<DashboardTotalsDto> Handle(GetDashboardTotalsQuery request, CancellationToken cancellationToken)
     {
-        var totalSales = await _context.Sales.SumAsync(x => x.TotalAmount, cancellationToken);
-        var totalCompletedOrders = await _context.Sales.CountAsync(cancellationToken);
+        var totalSales = await _context.Sales.Where(x => x.CampaignId == request.CampaignId)
+            .SumAsync(x => x.TotalAmount, cancellationToken);
+        var totalCompletedOrders = await _context.Sales
+            .Where(x => x.CampaignId == request.CampaignId)
+            .CountAsync(cancellationToken);
         var totalCommission = totalSales * 0.1m;
 
         return new DashboardTotalsDto
